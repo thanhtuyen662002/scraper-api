8b3ad6e5128a204f119cdd6ea4c9a7ac12e79ac1
Vo Thanh Tuyen <thanhtuyen66202@gmail.com>
2025-07-03 22:09:28 +0700

    fix link coffee

--- scrapers/kcoffee.js
+++ scrapers/kcoffee.js
@@ -12,7 +12,7 @@ module.exports = async function scrapeKCoffee() {
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
   );
 
-  await page.goto('https://www.kphucsinh.vn/collections/uu-dai', {
+  await page.goto('https://uudai.kphucsinh.vn/', {
     waitUntil: 'domcontentloaded',
     timeout: 60000
   });
--- scrapers/thecoffeehouse.js
+++ scrapers/thecoffeehouse.js
@@ -8,7 +8,7 @@ module.exports = async function scrapeTCH() {
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
   );
 
-  await page.goto('https://www.thecoffeehouse.com/pages/khuyen-mai', {
+  await page.goto('https://promothecoffeeehouse.com.vn/', {
     waitUntil: 'domcontentloaded',
     timeout: 60000
   });
