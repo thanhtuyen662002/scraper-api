953838319f2dfd56fe6d1d3ae271450d8bcb3f43
Vo Thanh Tuyen <thanhtuyen66202@gmail.com>
2025-07-03 21:43:34 +0700

    fix address of TCH and KC

--- scrapers/kcoffee.js
+++ scrapers/kcoffee.js
@@ -4,8 +4,8 @@ module.exports = async function scrapeKCoffee() {
   const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
   const page = await browser.newPage();
 
-  await page.goto('https://www.kphucsinh.vn/collections/uu-dai', { waitUntil: 'networkidle2' });
-  await page.waitForSelector('slide-img-wrapper img');
+  await page.goto('https://www.kphucsinh.vn/collections/uu-dai', { waitUntil: 'networkidle2', timeout: 120000 });
+  await page.waitForSelector('.slide-img-wrapper img');
 
   const images = await page.evaluate(() =>
     Array.from(document.querySelectorAll('img')).map(img => img.src)
--- scrapers/thecoffeehouse.js
+++ scrapers/thecoffeehouse.js
@@ -5,7 +5,7 @@ module.exports = async function scrapeTCH() {
   const page = await browser.newPage();
 
   await page.goto('https://www.thecoffeehouse.com/pages/khuyen-mai', { waitUntil: 'networkidle2' });
-  await page.waitForSelector('.owl-item img');
+  await page.waitForSelector('.item img');
 
   const images = await page.evaluate(() =>
     Array.from(document.querySelectorAll('img')).map(img => img.src)
